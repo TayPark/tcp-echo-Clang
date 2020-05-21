7b642190b936276e193d99ce6f03c62bb7a3ff6d
taypark <dodgeplay@naver.com>
2020-05-21 21:21:41 +0900

    one left

--- echo-client.c
+++ echo-client.c
@@ -11,7 +11,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
-#define BUFSIZE 10
+#define BUFSIZE 1460 // 이더넷의 사이즈는 1500바이트, TCP/IP 기본 헤더를 제외하면 40바이트이므로 TCP 세그먼트 payload 최대크기는 1460bytes로 설정
 #define SERVER_IP "127.0.0.1"
 #define ECHOPORT 9999
 
@@ -21,7 +21,7 @@ int main(int argc, char **argv) {
         int sock;
         struct sockaddr_in serv_addr;
         char message[BUFSIZE];
-        int str_len;
+        int str_len, times;
 
         // 클라이언트 소켓 생성부.IPv4, Stream 방식 정의
         sock = socket(PF_INET, SOCK_STREAM, 0);
@@ -30,7 +30,7 @@ int main(int argc, char **argv) {
                 error_handling("Socket creation error\n");
                 close(sock);
         }
-        
+        // sockaddr_in 초기화
         memset(&serv_addr, 0, sizeof(serv_addr));
 
         serv_addr.sin_family = AF_INET;
@@ -48,10 +48,12 @@ int main(int argc, char **argv) {
 
         while(1) {
                 fputs("Enter message return (Type 'exit' to quit) : ", stdout);
+                // paylaod 최대 크기만큼만 받는다. 넘을 경우 임시 저장소에 이를 집어넣고 최대 크기만큼 꺼내서 보내기로 하자.
                 fgets(message, BUFSIZE, stdin);
-
-                if (strlen(message) >= BUFSIZE) {
-                        printf("Overflow occured\n");
+                
+                // 입력 버퍼보다 메시지의 크기가 더 크다면 쪼개서 보냄
+                if(str_len >= strlen(message)) {
+                        
                 }
                 
                 // 종료 문구 기입시 종료
--- echo-server.c
+++ echo-server.c
@@ -17,14 +17,11 @@
 void error_handling(char *message);
 
 int main(int argc, char **argv) {
-        // 서버는 서버-클라이언트 소켓을 모두 가져야한다.
         int serv_sock, clnt_sock;
         char message[BUFSIZE];
         int str_len;
         struct sockaddr_in serv_addr, clnt_addr;
         int clnt_addr_size;
-
-
         // 서버 소켓 생성부. IPv4, Stream방식을 사용함을 정의. 
         serv_sock = socket(PF_INET, SOCK_STREAM, 0);  
         printf("Creating socket...\n");  
@@ -37,16 +34,19 @@ int main(int argc, char **argv) {
         memset(&serv_addr, 0, sizeof(serv_addr));
 
         serv_addr.sin_family = AF_INET;
-        // 주소 자동 지정
+        // 주소 자동 지정 및 포트 지정
         serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        // 바이트 순서 획일화
         serv_addr.sin_port = htons(ECHOPORT);
         printf("Binding...\n");
         // 소켓에 주소 할당 및 에러 핸들링. 실패시 close
         if( bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1 ) {
                 close(serv_sock);
-                error_handling("Binding error\n");
+                error_handling("Binding error: requested port is already in use\n");
         }
+
         // 바인딩 성공시 리스닝 상태로 진입. backlog가 0이면 default 값이나 로컬 에코 서버이므로 무의미.
+listen:
         if( listen(serv_sock, 0) == -1 )  {
                 close(serv_sock);
                 error_handling("Listening error\n");
@@ -58,23 +58,20 @@ int main(int argc, char **argv) {
         clnt_addr_size = sizeof(clnt_addr);
         
         // 커넥션 셋업 요청 수락 및 에러핸들링
-        printf("Connection requested: %d", clnt_sock);
         clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
         if(clnt_sock == -1) {
                 close(clnt_sock);
-                error_handling("Connection setup error: accpet()\n");
+                error_handling("Connection setup error: accept()\n");
         }
 
-        // 클라이언트로부터 메시지 읽은 값 처리
-        while((str_len = read(clnt_sock, message, BUFSIZE)) != 0 ) {
-                if (strlen(message) > BUFSIZE) { // 지정된 윈도우 사이즈보다 클 경우 잘라서 보낸다.
-                        
-                } else {
-                        write(clnt_sock, message, str_len);
-                        write(1, message, str_len);
-                }
-                
+        // 클라이언트로부터 받은 메시지 재전송
+        while((str_len = recv(clnt_sock, message, BUFSIZE, 0)) != 0 ) {
+                send(clnt_sock, message, str_len, 0);
+                send(1, message, str_len, 0);
         }
+// 연결이 끝나면 다시 리스닝 상태로 진입
+goto listen;
+
         /* 
           클라이언트가 종료요청을 했을 때 클라이언트 소켓 close. 
           서버는 다른 연결을 위해 프로그램이 끝날 때 까지 리스닝상태.
